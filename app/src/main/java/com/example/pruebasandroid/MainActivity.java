0d5d22122d0549788af7267d736312766629475d
Arturon <anegreiross@outlook.com>
2020-11-19 16:20:21 -0500

    new process added

--- app/src/main/java/com/example/pruebasandroid/MainActivity.java
+++ app/src/main/java/com/example/pruebasandroid/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     boolean bartShowing = true;
 
     public void fade(View view){
+        // log envia mensajes por consola
         Log log = null;
         log.i("Info", "Image view has been taped");
         // find by view is similar to docuemnt.getElementById xD
@@ -25,19 +26,29 @@ public class MainActivity extends AppCompatActivity {
         // ahora creamos una conficional de que si la imagen de Bart se muestra
         // entonces ocúltala, caso contrario escóndea, viceversa para Homero
 
+
+
+
+        // La fuincion animate().translationYBy() acepta valores positivos y negativos para mover
+        //  la imagen o devolverla también la función translationXBy
+
         if (bartShowing){
 
             bartShowing = false;
 
             bartimageView.animate().alpha(0).setDuration(2000);
             homerImageView.animate().alpha(1).setDuration(2000);
-            leslieImageView.animate().alpha(1).setDuration(10000);
+            //leslieImageView.animate().alpha(1).setDuration(10000);
+            bartimageView.animate().translationYBy(1000).setDuration(2000);
+            homerImageView.animate().translationXBy(-1000).setDuration(2000);
         } else{
 
             bartShowing = true;
 
             bartimageView.animate().alpha(1).setDuration(2000);
             homerImageView.animate().alpha(0).setDuration(2000);
+            bartimageView.animate().translationYBy(-1000).setDuration(2000);
+            homerImageView.animate().translationXBy(1000).setDuration(2000);
         }
 
 
