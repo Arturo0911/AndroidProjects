091327f7e90d7dff8fe3b8a67846d36ac179769d
Arturon <anegreiross@outlook.com>
2020-11-25 08:52:43 -0500

    created button activate

--- tictac/app/src/main/java/com/example/tictactoe/MainActivity.java
+++ tictac/app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     int [][] winningPositions= {{0,1,2}, {0,3,6},{0,4,8},{3,4,5},{6,7,8},{1,4,7},{2,5,8},{2,4,6}};
 
+    boolean activeGame = true;
+
 
 
     public void dropIn(View view){
