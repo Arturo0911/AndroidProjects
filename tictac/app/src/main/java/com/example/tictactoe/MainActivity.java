de91b3f6b79edfacf426da9322e36b002a94c262
Arturon <anegreiross@outlook.com>
2020-11-24 22:56:38 -0500

    added the method to appear the token

--- tictac/app/src/main/java/com/example/tictactoe/MainActivity.java
+++ tictac/app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -2,9 +2,31 @@ package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view){
+
+        /*
+        * set the value of the image without png image
+        * */
+        ImageView counter = (ImageView) view;
+        /*
+        * Now  set the position relative by Y
+        * search in drawable folder to found yellow image and set
+        * after that set animates like TranslationY by her original value 1500 and add rotation and setDuration
+        * */
+
+        counter.setTranslationY(-1500);
+        counter.setImageResource(R.drawable.yellow);
+        counter.animate().translationYBy(1500).setDuration(2000);
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
