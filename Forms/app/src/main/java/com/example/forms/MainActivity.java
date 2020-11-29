e9a4ceecfc9dc0af4db6ab22bd25f962799ff564
Arturon <anegreiross@outlook.com>
2020-11-29 17:38:38 -0500

    well done

--- Forms/app/src/main/java/com/example/forms/MainActivity.java
+++ Forms/app/src/main/java/com/example/forms/MainActivity.java
@@ -2,14 +2,40 @@ package com.example.forms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.example.routes.Routes;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText userName;
+    private EditText password;
+
+
+
+
     public void greetingInitial(View view){
-        Toast.makeText(this, "hola arturon", Toast.LENGTH_SHORT).show();
+        try {
+
+            userName = (EditText) findViewById(R.id.userName);
+            password = (EditText) findViewById(R.id.userPass);
+
+
+            if (!userName.getText().equals("") && !password.getText().equals("")){
+                Toast.makeText(this, "your user name is: "+userName.getText()+" and your pass is: "+password.getText(),
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, Routes.class);
+                startActivity(intent);
+            }
+
+        }catch (Exception e ){
+            Toast.makeText(this,"error by: "+e.toString(), Toast.LENGTH_SHORT ).show();
+        }
     }
 
     @Override
---
+++ Forms/app/src/main/java/com/example/routes/Routes.java
@@ -0,0 +1,16 @@
+package com.example.routes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.forms.R;
+
+public class Routes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_routes);
+    }
+}
