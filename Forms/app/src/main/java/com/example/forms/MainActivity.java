31185985012acf9fc01dc76cd6f3cf8f27b66c78
Arturon <anegreiross@outlook.com>
2020-11-29 16:58:44 -0500

    added new forms

--- Forms/app/src/main/java/com/example/forms/MainActivity.java
+++ Forms/app/src/main/java/com/example/forms/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.forms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void greetingInitial(View view){
+        Toast.makeText(this, "hola arturon", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
