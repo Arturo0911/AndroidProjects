c774d19de5a544392842784acedecccefb82c4f4
Arturon <anegreiross@outlook.com>
2020-12-01 14:22:32 -0500

    finished first part

--- Forms/app/src/main/java/com/example/cardio/CardioRegister.java
+++ Forms/app/src/main/java/com/example/cardio/CardioRegister.java
@@ -2,10 +2,30 @@ package com.example.cardio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import com.example.forms.*;
+import com.example.routes.Routes;
 
 public class CardioRegister extends AppCompatActivity {
+
+    //private Button initButoon;
+    private EditText nameText;
+
+
+    public void initProcess(View view){
+
+        Toast.makeText(CardioRegister.this, "Process initialazing", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this, Routes.class);
+        startActivity(intent);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
