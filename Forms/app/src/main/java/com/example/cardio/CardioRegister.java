ca10e97c60ac5cecdb14e49e1313cfe051e1b79e
Arturon <anegreiross@outlook.com>
2020-12-01 12:43:28 -0500

    new forms added

---
+++ Forms/app/src/main/java/com/example/cardio/CardioRegister.java
@@ -0,0 +1,15 @@
+package com.example.cardio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.forms.*;
+
+public class CardioRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardio_register);
+    }
+}
