7a97469cecc2f42613e9a0b029f13a8fc4b5aaba
Arturon <anegreiross@outlook.com>
2020-12-09 00:17:46 -0500

    ok

--- Forms/app/src/main/java/com/example/routes/Routes.java
+++ Forms/app/src/main/java/com/example/routes/Routes.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.forms.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Routes extends AppCompatActivity {
+
+    private ListView routinesList;
+    private ArrayList<String>vector;
+
+    public Routes(){
+        vector = new ArrayList<String>(Arrays.asList("Running", "Power"));
+        
+    }
 
 
     public void onGreeting(View view){
