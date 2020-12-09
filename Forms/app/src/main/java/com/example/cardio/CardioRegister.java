88c75c2eb30a9ca6fe41b7111016ba43b279119c
Arturon <anegreiross@outlook.com>
2020-12-08 23:12:59 -0500

    training before exam

--- Forms/app/src/main/java/com/example/cardio/CardioRegister.java
+++ Forms/app/src/main/java/com/example/cardio/CardioRegister.java
@@ -7,23 +7,46 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.example.forms.*;
 import com.example.routes.Routes;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import pl.droidsonroids.gif.GifAnimationMetaData;
 
 public class CardioRegister extends AppCompatActivity {
 
     //private Button initButoon;
     private EditText nameText;
+    //private  VideoView loadingVideo;
+    //private GifAnimationMetaData gifLoading ;
+
+    private LinearLayout linearLayouLoadingGift;
 
 
     public void initProcess(View view){
 
-        Toast.makeText(CardioRegister.this, "Process initialazing", Toast.LENGTH_SHORT).show();
 
-        Intent intent = new Intent(this, Routes.class);
-        startActivity(intent);
+
+        try {
+            linearLayouLoadingGift = (LinearLayout) findViewById(R.id.linearLayouLoadingGift);
+            linearLayouLoadingGift.setVisibility(view.VISIBLE);
+
+            TimeUnit.SECONDS.sleep(2);
+            Toast.makeText(CardioRegister.this, "Process initialazing", Toast.LENGTH_SHORT).show();
+
+
+            Intent intent = new Intent(this, Routes.class);
+            startActivity(intent);
+        }catch (Exception e){
+            Toast.makeText(this,"Erro by: "+e.toString(), Toast.LENGTH_SHORT).show();
+        }
 
     }
 
@@ -31,5 +54,7 @@ public class CardioRegister extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cardio_register);
+        //VideoView loadingVideo = (VideoView) findViewById(R.id.loadingGif);
+
     }
 }
--- Forms/app/src/main/java/com/example/routes/Routes.java
+++ Forms/app/src/main/java/com/example/routes/Routes.java
@@ -3,10 +3,17 @@ package com.example.routes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.forms.R;
 
 public class Routes extends AppCompatActivity {
+
+
+    public void onGreeting(View view){
+        Toast.makeText(Routes.this, "hola chicos, espero podamos pasar todas las materias xD", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
