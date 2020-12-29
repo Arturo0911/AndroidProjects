71492cadffc864798f3a17fc79ebfdc0c044ae3a
Arturon <anegreiross@outlook.com>
2020-12-29 18:57:30 -0500

    finished first activity

---
+++ shopproducts/app/src/main/java/com/example/shop_products/IndexTemplate.java
@@ -0,0 +1,14 @@
+package com.example.shop_products;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IndexTemplate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index_template);
+    }
+}
