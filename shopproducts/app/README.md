d60488bbafc91cb6e1d6d5f4352fd49a8a665088
Arturon <anegreiross@outlook.com>
2020-12-29 16:25:56 -0500

    material design added

---
+++ shopproducts/app/README.md
@@ -0,0 +1,17 @@
+Estimados estudiantes en el Taller #1 se  debe desarrollar dos pantallas funcionales que mostrará los siguientes elementos:
+TextView,
+edittext,
+ImageView,
+Spinner,
+RadioButton,
+Checkboxes,
+Button,
+
+donde deben de incluir diseño e investigación del mismo.
+
+Pantalla. (Producto, Vehiculo, Tienda Online)
+ingresar las descripciones de los productos, seleccionar con las diferentes elementos
+(Spinner, RadioButton,Checkboxes), Accionar Boton (Comprar o enviar) a la siguiente Pantallas.
+
+Pantalla (Mostrar lo que realizó en la primera Pantalla)
+Mostrar los Datos que se escogieron en la primera pantalla.
--- shopproducts/app/src/main/java/com/example/shop_products/MainActivity.java
+++ shopproducts/app/src/main/java/com/example/shop_products/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.shop_products;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    ActionBar actionBar;
+
+
+    public void generatePurchase (View view){
+        Toast.makeText(this, "Purchase generated", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        actionBar  = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#273036")));
+        actionBar.setTitle("CAR SHOP");
+
     }
 }
