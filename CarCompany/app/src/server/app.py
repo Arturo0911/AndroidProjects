1d622e303c89d3c2919439b0f4b7801f056a9842
Arturon <anegreiross@outlook.com>
2021-01-17 18:46:50 -0500

    still defining signup route

--- CarCompany/app/src/server/app.py
+++ CarCompany/app/src/server/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_mail import Mail, Message
 from flask import jsonify, request, Response
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from flask_pymongo import PyMongo
 
 
 app = Flask(__name__)
@@ -12,36 +12,77 @@ app.config['MAIL_USERNAME'] = 'sapo@gmail.com'
 app.config['MAIL_PASSWORD'] = '**********************'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
+# connection with Mongodb
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/CarCompany'
+mongo = PyMongo(app)
 mail = Mail(app)
 
 
-
-
-@app.route("/", methods=['GET','POST'])
+# route to login
+@app.route("/", methods=['POST'])
 def index():
     
-    if request.method = '':
+    if request.method = 'POST':
         
         username = request.json['Username']
         password = request.json['Password']
         
         try:
-            if username and password:
-                pass
+            if username  and password:
+                
+                
+
+
             else:
+                pass
         except Exception as e:
 
             return jsonify({
                     'status': str(e)
                 })
-
-
-
     else:
         pass
 
 
+# route to signup
+@app.route("/signup", methods=['POST'])
+def signup():
+    
+    if request.method == 'POST':
 
+        try:
+            credentials = request.json['Credentials']
+            name = request.json['Name']
+            last_name = request.json['LastName']
+            email = request.json['Email']
+            phone_number = request.json['PhoneNumber']
+            address = request.json['Address']
+            username = request.json['Username']
+            password = generate_password_hash(request.json['Password']) # Saved password into the database
+            object_id = mongo.db.users.insert({
+                'identification':credentials,
+                'names':name,
+                'last_name':last_name,
+                'email': email,
+                'phone_number':phone_number,
+                'address': address,
+                'username':username,
+                'password':password
+                })
+
+
+            # after this one, the data will be saved into the db Mongo
+            # object response
+            response.jsonify({'message': 'saved successfully'})
+            response.status_code = 200
+            return reponse
+
+
+        except Exception as e:
+            
+            return jsonify({'status':str(e)})
+    else:
+        return jsonify({'status': 'bad request'})
 
 
 
